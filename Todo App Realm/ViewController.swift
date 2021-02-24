9d99d9184f6c4307416a9d8edf9a94b34f7e96b3
sadiq <sadiq.qasmi3@gmail.com>
2021-02-24 06:06:18 +0500

    Initial Commit

---
+++ Todo App Realm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todo App Realm
+//
+//  Created by sadiq qasmi on 24/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
